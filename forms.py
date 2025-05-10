47c7011eec635cf534164fe54235c3362493594f
ley_ <lltenebroso@up.edu.ph>
2025-05-10 20:44:30 +0800

    Deleted forms.py

--- forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class PostForm(FlaskForm):
-    message = StringField('message', validators=[DataRequired()])
